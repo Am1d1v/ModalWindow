9a0c994696829a07cae34597f06f58bcfbb94c7a
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-05-30 00:26:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const modalWindow = document.querySelector(".hidden");
 
 const closeBtnModalWindow = document.querySelector(".hidden__Btn");
 
+const modalWindowAll = document.querySelectorAll(".modalbtn");
 const modalWindowBtn1 = document.querySelectorAll(".modalbtn")[0];
 const modalWindowBtn2 = document.querySelectorAll(".modalbtn")[1];
 const modalWindowBtn3 = document.querySelectorAll(".modalbtn")[2];
+
+
+for( let i = 0; i < modalWindowAll.length; i ++){
+    modalWindowAll[i].addEventListener("click", () => {
+        console.log(modalWindowAll[i].textContent);
+        modalWindow.style.display = "block";
+    }); 
+    
+}
+
+closeBtnModalWindow.addEventListener("click",() => {
+    modalWindow.style.display = "none";
+});
