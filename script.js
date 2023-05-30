d55170dcf0159be0c446096369f4265cc9aae9ab
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-05-30 18:28:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
-const modalWindow = document.querySelector(".hidden");
-
-const closeBtnModalWindow = document.querySelector(".hidden__Btn");
+const modalWindow = document.querySelector(".hidden"); // Modal Window
+const closeBtnModalWindow = document.querySelector(".hidden__Btn"); // Close Window Button
+const overlay = document.querySelector(".hidden__overlay");
 
 const modalWindowAll = document.querySelectorAll(".modalbtn");
 const modalWindowBtn1 = document.querySelectorAll(".modalbtn")[0];
@@ -10,7 +10,7 @@ const modalWindowBtn2 = document.querySelectorAll(".modalbtn")[1];
 const modalWindowBtn3 = document.querySelectorAll(".modalbtn")[2];
 
 
-for( let i = 0; i < modalWindowAll.length; i ++){
+for( let i = 0; i < modalWindowAll.length; i ++){ // All Modal Buttons
     modalWindowAll[i].addEventListener("click", () => {
         console.log(modalWindowAll[i].textContent);
         modalWindow.style.display = "block";
@@ -18,6 +18,24 @@ for( let i = 0; i < modalWindowAll.length; i ++){
     
 }
 
-closeBtnModalWindow.addEventListener("click",() => {
+
+const closeModalWindow = function(){
+    modalWindow.style.display = "none";
+}
+
+closeBtnModalWindow.addEventListener("click", closeModalWindow);
+
+overlay.addEventListener("click", closeModalWindow);
+
+
+/*
+
+closeBtnModalWindow.addEventListener("click",() => { // Close Modal Window
     modalWindow.style.display = "none";
 });
+
+overlay.addEventListener("click",() => { // Close Modal Window by clicking on overlay
+    modalWindow.style.display = "none";
+})
+
+*/
